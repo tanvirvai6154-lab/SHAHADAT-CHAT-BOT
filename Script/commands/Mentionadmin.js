c11100242d74206ed4d144bfbba14ed3617355dd
srovi2007akash-stack <srovi2007akash@gmail.com>
2025-09-03 18:24:34 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["100078049308655", "61552132761819", "61579763644616"].map(String);
+ const adminIDs = ["100078049308655", "61552132761819", "61562187727716"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
