9acfefa06378e8ec7076e1028df00c500f65e2c2
srovi2007akash-stack <srovi2007akash@gmail.com>
2025-09-07 15:15:01 +0600

    Update Emojivoice.js

--- Script/commands/Emojivoice.js
+++ Script/commands/Emojivoice.js
@@ -48,7 +48,8 @@ const emojiAudioMap = {
  "💋": "https://files.catbox.moe/37dqpx.mp3",
  "🫦": "https://files.catbox.moe/61w3i0.mp3",
  "😴": "https://files.catbox.moe/rm5ozj.mp3",
- "🙏": "https://files.catbox.moe/7avi7u.mp3"
+ "🙏": "https://files.catbox.moe/7avi7u.mp3",
+ "😼": "https://files.catbox.moe/4oz916.mp3"
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
