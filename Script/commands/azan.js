dfaeea752e75e2c02fd127cdbd4e61fa507fa843
srovi2007akash-stack <srovi2007akash@gmail.com>
2025-09-10 16:02:10 +0600

    Update azan.js

--- Script/commands/azan.js
+++ Script/commands/azan.js
@@ -1,54 +1,78 @@
 // azan.js
-// Auto Azan notification in ALL groups
-// File: modules/commands/azan.js
-
+// Auto Azan notification in ALL groups (Real-time from API)
+const axios = require("axios");
 const schedule = require("node-schedule");
 
 module.exports.config = {
-  name: "azan",
-  version: "3.0.0",
-  hasPermssion: 0,
-  credits: "Akash",
-  description: "প্রতিদিন আজানের সময় সব গ্রুপে অটো নোটিফিকেশন পাঠাবে",
-  commandCategory: "Islamic",
-  usages: "অটো রান",
-  cooldowns: 5
+    name: "azan",
+    version: "4.0.0",
+    hasPermssion: 0,
+    credits: "Akash",
+    description: "প্রতিদিন আজানের সঠিক সময় সব গ্রুপে অটো নোটিফিকেশন পাঠাবে",
+    commandCategory: "Islamic",
+    usages: "অটো রান",
+    cooldowns: 5
 };
 
 let jobs = [];
 
+// আজানের জন্য API থেকে টাইম নিয়ে শিডিউল সেট করা
+async function scheduleAzan(api) {
+    try {
+        // বাংলাদেশের আজানের সময়ের API
+        const res = await axios.get("http://api.aladhan.com/v1/timingsByCity", {
+            params: {
+                city: "Dhaka",
+                country: "Bangladesh",
+                method: 2 // Islamic Society of North America
+            }
+        });
+
+        const timings = res.data.data.timings;
+
+        // আগের সব জব বন্ধ করা
+        jobs.forEach(job => job.cancel());
+        jobs = [];
+
+        // আজানের টাইম অনুযায়ী নতুন জব তৈরি
+        for (const [prayer, time] of Object.entries(timings)) {
+            // শুধুমাত্র ৫টি নামাজের জন্য
+            if (["Fajr", "Dhuhr", "Asr", "Maghrib", "Isha"].includes(prayer)) {
+                let [hour, minute] = time.split(":").map(Number);
+
+                // ঢাকার টাইম অনুযায়ী শিডিউল
+                const rule = new schedule.RecurrenceRule();
+                rule.tz = "Asia/Dhaka";
+                rule.hour = hour;
+                rule.minute = minute;
+
+                const job = schedule.scheduleJob(rule, function() {
+                    const msg = `__আসসালামু আলাইকুম 🕌__\n${prayer} এর আজান দিচ্ছে...!! সবাই ওযু করে নামাজের জন্য প্রস্তুত হও ইনশাআল্লাহ 🤲`;
+
+                    for (const threadID of global.data.allThreadID) {
+                        api.sendMessage(msg, threadID);
+                    }
+                });
+
+                jobs.push(job);
+            }
+        }
+
+        console.log("✅ আজান নোটিফিকেশন সিস্টেম চালু হয়েছে (সঠিক সময় অনুযায়ী)!");
+    } catch (err) {
+        console.error("❌ আজান টাইম নেওয়ার সময় সমস্যা হয়েছে:", err.message);
+    }
+}
+
 module.exports.onLoad = async function({ api }) {
-  // আজানের টাইম (বাংলাদেশ স্ট্যান্ডার্ড টাইম)
-  const prayerTimes = {
-    "ফজর": "04:40",
-    "যোহর": "12:10",
-    "আসর": "15:30",
-    "মাগরিব": "18:15",
-    "এশা": "19:45"
-  };
+    await scheduleAzan(api);
 
-  for (let [prayer, time] of Object.entries(prayerTimes)) {
-    const [hour, minute] = time.split(":").map(Number);
-
-    const job = schedule.scheduleJob({ hour, minute, tz: "Asia/Dhaka" }, function () {
-      const msg = 
-`__আসসালামু আলাইকুম 🕌__
-
-${prayer} এর আজান দিচ্ছে...!!
-সবাই ওযু করে নামাজের জন্য প্রস্তুত হও ইনশাআল্লাহ 🤲`;
-
-      // সব গ্রুপে পাঠাবে
-      for (const threadID of global.data.allThreadID) {
-        api.sendMessage(msg, threadID);
-      }
+    // প্রতি ২৪ ঘন্টায় API থেকে নতুন সময় নিয়ে শিডিউল আপডেট করবে
+    schedule.scheduleJob("0 0 * * *", function() {
+        scheduleAzan(api);
     });
-
-    jobs.push(job);
-  }
-
-  console.log("✅ আজান নোটিফিকেশন সিস্টেম চালু হয়েছে (সব গ্রুপে)।");
 };
 
 module.exports.run = async function() {
-  // কোনো কমান্ড দরকার নেই, অটো চলবে
+    // অটো রান করবে, কোনো কমান্ডের দরকার নেই
 };
